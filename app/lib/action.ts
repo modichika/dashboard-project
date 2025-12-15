02d358448ab0c91ab6c28de11eb038cb88494fff
Shristi modi <shristimodi95@gmail.com>
2025-12-15 18:23:33 +0530

    added authentication

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,8 @@ import {z} from 'zod';
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
 import postgres from 'postgres';
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require'});
 
@@ -68,12 +70,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 
-export async function updateInvoice(id: string, formData: FormData){
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, formData: FormData, prevState: State){
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -83,9 +94,9 @@ export async function updateInvoice(id: string, formData: FormData){
   WHERE id = ${id}
 `;
     } catch (error) {
-        console.error(error);
+
         return {
-            message: 'Failed to update invoice',
+            message: 'Database Error: Failed to Update Invoice.',
         }
     }
 
@@ -100,4 +111,23 @@ export async function deleteInvoice(id: string){
      await sql `DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -49,6 +49,15 @@ export default function EditInvoiceForm({
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+            <div id="customer-error" aria-live="polite" aria-atomic="true">
+                {state.errors?.customerId &&
+                    state.errors.customerId.map((error: string) => (
+                        <p className="mt-2 text-sm text-red-500" key={error}>
+                            {error}
+                        </p>
+                    ))}
+
+            </div>
         </div>
 
         {/* Invoice Amount */}
@@ -69,6 +78,14 @@ export default function EditInvoiceForm({
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+              <div id="amount-error" aria-live="polite" aria-atomic="true">
+                  {state.errors?.amount &&
+                      state.errors.amount.map((error: string) => (
+                          <p className="mt-2 text-sm text-red-500" key={error}>
+                              {error}
+                          </p>
+                      ))}
+              </div>
           </div>
         </div>
 
@@ -113,7 +130,20 @@ export default function EditInvoiceForm({
               </div>
             </div>
           </div>
+            <div id="status-error" aria-live="polite" aria-atomic="true">
+                {state.errors?.status &&
+                    state.errors.status.map((error: string) => (
+                        <p className="mt-2 text-sm text-red-500" key={error}>
+                            {error}
+                        </p>
+                    ))}
+            </div>
         </fieldset>
+          <div aria-live="polite" aria-atomic="true">
+              {state.message ? (
+                  <p className="mt-2 text-sm text-red-500">{state.message}</p>
+              ) : null}
+          </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
